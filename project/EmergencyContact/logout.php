7a241ec1c0933685c3e726fa04d8f10768848dc4
ifkame <93113173+ifkame@users.noreply.github.com>
2022-04-25 11:57:35 +0900

    Delete project/EmergencyContact directory

--- project/EmergencyContact/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();        //セッション開始
-$_SESSION = array();    //セッション破棄
-session_destroy();      //セッション停止
-
-// リダイレクトを実行(headerが読み込まれる前に呼び出す)
-header("Location: ./login.php"); //ログインに遷移
-exit();
